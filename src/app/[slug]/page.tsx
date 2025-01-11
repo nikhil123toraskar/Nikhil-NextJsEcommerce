1afc337a76c7a4d86b6a00faae7f9474d51aa7fb
nikhil123toraskar <nikhil123toraskar@gmail.com>
2025-01-11 12:22:09 +0530

    deployment issue 1

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -59,7 +59,7 @@ const SinglePage = async ({params} : {params: {slug: string}}) => {
             productOptions={product.productOptions!}
           />
         ) : (
-          <Add productId={product._id} variantID = "00000000-0000-0000-000000000000" stockNumber={product.stock?.quantity || 0}></Add>
+          <Add productId={product._id!} variantId = "00000000-0000-0000-000000000000" stockNumber={product.stock?.quantity || 0}></Add>
         )}
 
         <div className="h-[2px] bg-gray-100"></div>

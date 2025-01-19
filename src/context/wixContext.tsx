31edd252b672aa91a5701fcdcd81278192bab2c9
nikhil123toraskar <nikhil123toraskar@gmail.com>
2025-01-19 10:08:13 +0530

    Revert "clientID testing"
    This reverts commit f91cf91a549b7f82f888566d63cc26d6977166ba.

--- src/context/wixContext.tsx
+++ src/context/wixContext.tsx
@@ -18,7 +18,7 @@ const wixClient = createClient({
     redirects
   },
   auth: OAuthStrategy({
-    clientId: '5604bf31-0355-43b0-8c7a-6f8d1265f8db',
+    clientId: process.env.NEXT_PUBLIC_WIX_CLIENTID!,
     tokens: {
       refreshToken,
       accessToken: { value: "", expiresAt: 0 },
